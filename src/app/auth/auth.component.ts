9a76174281a885f5cf3aa29abb21129e7db61332
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-02-25 00:15:27 +0200

    Sending Signin requests

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styles: [``]
 })
 export class AuthComponent {
 
@@ -30,25 +31,26 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObservable:Observable<AuthResponseData>;
+
     this.isLoading = true;
 
-    if (this.isLoginMode) {
-      // *code here to sign in
-    }
+    if (this.isLoginMode)
+      authObservable = this.authService.login(email, password)
     else 
-      this.authService
-        .signup(email, password)
-        .subscribe(
-          responseData => {
-            console.log(responseData);
-            this.isLoading = false;
-          },
-          errorMessage => {
-            console.log(errorMessage);
-            this.error = errorMessage;
-            this.isLoading = false;
-          }
-        )
+      authObservable = this.authService.signup(email, password)
+    
+    authObservable.subscribe(
+      responseData => {
+        console.log(responseData);
+        this.isLoading = false;
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { Observable, catchError, throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken:string;
     email:string;
     refreshToken:string;
     expiresIn:string;
     localId:string;
+    registered?:boolean;
 }
 
 @Injectable({
@@ -16,38 +17,51 @@ interface AuthResponseData {
 export class AuthService {
 
     apiKey:string = ' AIzaSyAOt3HXBDAY2BV0e6rMWAR35NQJ25mfdTE ';
-    url:string = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey;
+    signUpUrl:string = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey;
+    signInUrl:string = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.apiKey;
 
     constructor(
         private http:HttpClient,
     ) {}
 
-    signup( email:string, password:string, ) {
+    signup( email:string, password:string ): Observable<AuthResponseData> {
+        const payload = { email: email, password: password, returnSecureToken: true };
         return this.http
-            .post<AuthResponseData>(
-                this.url,
-                {
-                    email: email,
-                    password: password,
-                    returnSecureToken: true
-                }
-            )
-            .pipe(
-                catchError(
-                    errorResponse => {
-                        let errorMessage = 'An unknown error occured!'
-
-                        if (!errorResponse.error || !errorResponse.error.error)
-                            return throwError(errorMessage)
-
-                        if (errorResponse.error.error.message === 'EMAIL_EXISTS')
-                            errorMessage = 'The email address is already in use by another account.';
-
-                        return throwError(errorMessage)
-                    }
-                )
-            )
+        .post <AuthResponseData>( this.signUpUrl, payload )
+        .pipe( catchError( this.handleError) )
     }
 
-}
+    login( email:string, password:string ): Observable<AuthResponseData> {
+        const payload = { email: email, password: password, returnSecureToken: true };
+        return this.http
+        .post <AuthResponseData>( this.signInUrl, payload )
+        .pipe( catchError( this.handleError) )
+    }
 
+    private handleError( errorResponse:HttpErrorResponse ) {
+
+        let errorMessage = 'An unknown error occured!'
+        if (!errorResponse.error || !errorResponse.error.error)
+            return throwError(() => errorMessage)
+    
+        const message = errorResponse.error.error.message;
+        const manyAttempts:string = 'TOO_MANY_ATTEMPTS_TRY_LATER : Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.';
+    
+        //signup
+        if (message === 'EMAIL_EXISTS')
+            errorMessage = 'The email address is already in use by another account.';
+        
+        if (message === manyAttempts)
+          errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+    
+        //login
+        if (message === 'INVALID_LOGIN_CREDENTIALS')
+            errorMessage = 'Incorrect credentials. Please try again.';
+    
+        if (message === 'USER_DISABLED')
+            errorMessage = 'This user account has been disabled by an administrator.';
+    
+        return throwError(() => errorMessage)
+      }
+
+}
