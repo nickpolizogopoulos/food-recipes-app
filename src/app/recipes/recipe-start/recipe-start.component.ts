39c1aff9897fb765414ab35098f5847450db249e
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-03-08 21:44:33 +0200

    Added Instructions

--- src/app/recipes/recipe-start/recipe-start.component.ts
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -2,7 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-start',
-  templateUrl: './recipe-start.component.html',
+  template: `
+  
+    <ol class="lead">
+      <li>
+          <p>Select <u>Manage</u> at the top right and then Fetch Data.</p>
+      </li>
+      <li>
+          <p>Once the recipe list is fetched, select a recipe to read, edit or delete from the recipe list.</p>
+      </li>
+      <li>
+          <p>When you finish, select <u>Manage</u> and then <u>Save</u> to save the changes!</p>
+      </li>
+    </ol>
+  
+  `,
   styles: [``]
 })
 export class RecipeStartComponent {
